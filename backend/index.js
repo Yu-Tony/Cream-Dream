996b9c14d6c7fcb136239775c22467fb8c9785de
Leonardo <aaronleonidas@gmail.com>
2022-02-23 00:23:59 -0600

    Cliente v1

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,12 @@ const app = express();
 const port = 5000;
 require("./models/connection");
 
+/* requiere's de las rutas */
+const cliente_router = require("./routes/ClienteRoutes");
+
 app.use(bodyParser.json());
+
+app.use("/api", cliente_router);
 
 app.listen(port, () => {
   console.log("La aplicación está escuchando al puerto " + port);
---
+++ backend/models/ClienteSchema.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const clienteSchema = new mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true,
+  },
+  apellido: {
+    type: String,
+    required: true,
+  },
+  correo: {
+    type: String,
+    required: true,
+  },
+  contraseña: {
+    type: String,
+    required: true,
+  },
+});
+
+const Cliente = mongoose.model("cliente", clienteSchema);
+module.exports = Cliente;
