bf0001c8e86f6aafe947e92f7e853e1838f71924
Leonardo <aaronleonidas@gmail.com>
2022-02-26 13:12:56 -0600

    Archivo de constantes

---
+++ backend/constants.js
@@ -0,0 +1,7 @@
+exports.error = "Error";
+exports.data_founds = "Data found";
+exports.not_found = "Not found";
+exports.updated_success = "Updated successfully";
+exports.created_success = "Created successfully";
+exports.deleted_success = "Deleted successfully";
+exports.request_submitted = "Request submitted successfully";
