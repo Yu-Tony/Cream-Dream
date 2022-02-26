7140d7f58b06f02e5b6c372cf0f09561dcf9c26f
Leonardo <aaronleonidas@gmail.com>
2022-02-26 13:12:56 -0600

    Archivo de constantes

---
+++ backend/constants.js
@@ -0,0 +1,7 @@
+const error = "Error";
+const data_founds = "Data found";
+const not_found = "Not found";
+const updated_success = "Updated successfully";
+const created_success = "Created successfully";
+const deleted_success = "Deleted successfully";
+const request_submitted = "Request submitted successfully";
