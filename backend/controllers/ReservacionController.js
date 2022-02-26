d10098e146ad2d2968a09d04db73a38141299410
Leonardo <aaronleonidas@gmail.com>
2022-02-25 23:57:25 -0600

    nombre de los campos modificados

--- backend/controllers/ReservacionController.js
+++ backend/controllers/ReservacionController.js
@@ -1,48 +1,52 @@
 const Reservacion = require("../models/ReservacionSchema");
 
 exports.reservacion_create = async (req, res) => {
-    const {body} = req; 
+  const { body } = req;
 
-    //Aqui deberia de ir la validación de la información.
+  //Aqui deberia de ir la validación de la información.
 
-    await newReservacion //Funciones:
-        .save() //Función si ese objeto modelo ya existe, lo actualiza; si es un objeto nuevo, lo inserta.
-        .then((newObject) => console.log("Reservación creada con exito", newObject))
-        .catch((err) => {
-            console.error("Oops! Error al querer hacer una reservación", err)
-            res.send(err.errors);
-        });
+  await newReservacion //Funciones:
+    .save() //Función si ese objeto modelo ya existe, lo actualiza; si es un objeto nuevo, lo inserta.
+    .then((newObject) => console.log("Reservación creada con exito", newObject))
+    .catch((err) => {
+      console.error("Oops! Error al querer hacer una reservación", err);
+      res.send(err.errors);
+    });
 
-    res.send(newReservacion); 
+  res.send(newReservacion);
 };
 
-
 exports.reservacion_delete = async (req, res) => {
-    const {id} = req.params;
+  const { id } = req.params;
 
-    await Reservacion.deleteOne({_id: id});
+  await Reservacion.deleteOne({ _id: id });
 
-    res.send({message: "Reservación eliminada"})
+  res.send({ message: "Reservación eliminada" });
 };
 
 exports.reservacion_getById = async (req, res) => {
-    const {id} = req.params;
-    const data = await Reservacion.findById(id); 
+  const { id } = req.params;
+  const data = await Reservacion.findById(id);
 
-    if(data){
-        res.send(data);
-    }else{
-        res.send({message: "La reservación no existe"});
-    }
+  if (data) {
+    res.send(data);
+  } else {
+    res.send({ message: "La reservación no existe" });
+  }
 };
 
-exports.reservacion_getByPeopleDateSucursal = async (req, res) => { 
-    const {p, f, h, s} = req.params;
-    const data = await Reservacion.find({No_Personas: p, fecha: f, hora: h, mesa:s});
+exports.reservacion_getByPeopleDateSucursal = async (req, res) => {
+  const { p, f, h, s } = req.query;
+  const data = await Reservacion.find({
+    no_Personas: p,
+    fecha: f,
+    hora: h,
+    mesa: s,
+  });
 
-    if(data){
-        res.send(data);
-    }else{
-        res.send({message: "No existe reservación con los datos ingresados"});
-    }
+  if (data) {
+    res.send(data);
+  } else {
+    res.send({ message: "No existe reservación con los datos ingresados" });
+  }
 };
--- backend/routes/ReservacionRoutes.js
+++ backend/routes/ReservacionRoutes.js
@@ -1,11 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const reservacion_controller = require('../controllers/ReservacionController');
+const reservacion_controller = require("../controllers/ReservacionController");
 
 router.post("/reservacion", reservacion_controller.reservacion_create);
 router.delete("/reservacion/:id", reservacion_controller.reservacion_delete);
 router.get("/reservacion/:id", reservacion_controller.reservacion_getById);
-router.get("/reservacion?p='personas'&f='fecha'&h='hora'&s='sucursal'", reservacion_controller.reservacion_getByPeopleDateSucursal);
+router.get(
+  "/reservacion",
+  reservacion_controller.reservacion_getByPeopleDateSucursal
+);
 
 module.exports = router;
